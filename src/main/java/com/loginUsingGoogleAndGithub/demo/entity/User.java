be81a6a2e58e947f23f6939c90bd4951e900ef90
Deep Chakraborty <deep123aeccse@gmail.com>
2025-02-17 00:14:34 +0530

    First commit

---
+++ src/main/java/com/loginUsingGoogleAndGithub/demo/entity/User.java
@@ -0,0 +1,54 @@
+package com.loginUsingGoogleAndGithub.demo.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users") // Ensure the correct table name
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String provider;
+    private String providerId;
+}
